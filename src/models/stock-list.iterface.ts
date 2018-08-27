9f41dcd038645b94e61773a6b5758892a1dabd5d
ansh.ved@sakec.ac.in <ansh.ved@sakec.ac.in>
2018-08-28 00:03:11 +0530

    adding item and storing in database done, radio buttons not working yet

---
+++ src/models/stock-list.iterface.ts
@@ -0,0 +1,7 @@
+export interface StockList{
+itemName: string;
+itemColour: string;
+itemSize: string;
+itemQuantity: string;
+boysGirls: string;
+}
--- src/pages/add-item/add-item.ts
+++ src/pages/add-item/add-item.ts
@@ -1,25 +1,27 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AddItemPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { StockList } from "../../models/stock-list.iterface";
+import { AngularFireDatabase } from "angularfire2/database";
 @IonicPage()
 @Component({
-  selector: 'page-add-item',
-  templateUrl: 'add-item.html',
+  selector: "page-add-item",
+  templateUrl: "add-item.html"
 })
 export class AddItemPage {
+  stockList = {} as StockList;
+  stockListRef$: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private database: AngularFireDatabase
+  ) {
+    this.stockListRef$ = this.database.list("stockList")
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddItemPage');
+  addItem(stockList: StockList){
+this.stockListRef$.push(stockList).then(() =>{
+  console.log("succsessfully pushed");
+});
   }
-
 }
